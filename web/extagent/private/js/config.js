a2401f094a60ab5ea2928cbe468a3b7c96629d7e
Krunoslav Peric <perick@flip3.engr.oregonstate.edu>
2017-11-19 19:00:02 -0800

    deleted configs

--- web/extagent/private/js/config.js
+++
@@ -1,4 +0,0 @@
-var serverPorts = {
-    serverPort   : 17001
-}
-module.exports.serverPorts = serverPorts;
--- web/extagent/private/js/dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : '',
-  user            : '',
-  password        : '',
-  database        : ''
-});
-module.exports.pool = pool;
